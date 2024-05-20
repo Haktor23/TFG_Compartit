3d89f95555f4241ab0013714b3e5156cf0050ca2
Haktor23 <hecburmar@alu.edu.gva.es>
2024-05-20 18:21:09 +0200

    Footer i header acabat

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -9,14 +9,5 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  menuValue: boolean = false;
-  menu_icon: string = 'bi bi-list';
-  openMenu() {
-    this.menuValue = !this.menuValue;
-    this.menu_icon = this.menuValue ? 'bi bi-x' : 'bi bi-list';
-  }
-  closeMenu() {
-    this.menuValue = false;
-    this.menu_icon = 'bi bi-list';
-  }
+
 }
